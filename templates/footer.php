86bbd70b636a4aea18f6fe2f0a6f84d8bf37e3cf
A. C. Raven <acrogelio123@gmail.com>
2025-11-05 19:32:03 +0800

    pooter
    Updated footer layout and styles for improved design.

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,63 @@
-<footer class="section">
-	<div class="center grey-text">Copyright 2025 Ninja Pizzas</div>
-	
-</footer>
+<footer class="page-footer blue lighten-3" style="margin:0; padding:0;">
+  <div class="container black-text" style="margin-bottom:0;">
+    <div class="row" style="margin-bottom:0;">
+      <div class="col l6 s12">
+        <h5>Travel more ewan</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="col l4 offset-l2 s12">
+        <h5>Official links</h5>
+        <ul style="margin-bottom:0; list-style:none; padding-left:0;">
+          <li>TOURS TRAVELS</li>
+          <li>CURRENCY EXCHANGE</li>
+          <li>HOTELS</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-</body>
+  <div class="footer-copyright">
+    <div class="container center black-text" style="margin-bottom:0;">
+      TOURS @ URSAC2025
+    </div>
+  </div>
+
+  <style>
+    html, body {
+      margin: 0 !important;
+      padding: 0 !important;
+      height: 100%;
+    }
+
+    body {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+
+    .container {
+      flex: 1 0 auto;
+    }
+
+    footer.page-footer {
+      margin-top: auto !important;
+      padding: 0 !important;
+      border: none;
+    }
+
+    .page-footer .row,
+    .page-footer p,
+    .page-footer ul {
+      margin-bottom: 0 !important;
+    }
+
+    body::after {
+      content: "";
+      display: block;
+      height: 0;
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+</footer>
